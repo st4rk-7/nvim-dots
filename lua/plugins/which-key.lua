213ab4a4c2752c77ff96e70b708d4c40b7ff0e85
st4rk-7 <st4rk.7@proton.me>
2024-11-25 19:56:01 +0500

    change snippet + autoformat cpp

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -63,7 +63,7 @@ return {
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
-        { '<leader>t', group = '[T]oggle' },
+        { '<leader>u', group = 'Toggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       },
     },
