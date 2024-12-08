d43152d3bb51bed71c75ec990e0c23e620a416dd
st4rk-7 <st4rk.7@proton.me>
2024-12-08 12:05:16 +0500

    add shellcheck linter

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -8,6 +8,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         -- markdown = { 'markdownlint' },
+        sh = { 'shellcheck' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
